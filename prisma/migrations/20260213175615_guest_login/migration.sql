57640a2cfc7bf88939f1fef4ff2d087efee61a43
rabbitCase <gnair3004@gmail.com>
2026-02-14 00:11:39 +0530

    anon migration

---
+++ prisma/migrations/20260213175615_guest_login/migration.sql
@@ -0,0 +1,11 @@
+-- AlterTable
+ALTER TABLE "user" ADD COLUMN     "isAnonymous" BOOLEAN DEFAULT false;
+
+-- CreateIndex
+CREATE INDEX "account_userId_idx" ON "account"("userId");
+
+-- CreateIndex
+CREATE INDEX "session_userId_idx" ON "session"("userId");
+
+-- CreateIndex
+CREATE INDEX "verification_identifier_idx" ON "verification"("identifier");
